378b55e4c866255b0c1df221941dee748a5a5ac4
Jaslavie <jasmineqw45@gmail.com>
2026-06-12 01:15:55 -0700

    fix: strafe and left/right actions commented out

--- Malmo/py27/lewm_integration_layer_py27.py
+++ Malmo/py27/lewm_integration_layer_py27.py
@@ -43,7 +43,6 @@ def process_LeWM_action(action_list, current):
     if move != current[0] - current[2]:
         instructions.append("move {0}".format(move))
 
-    # Strafe disabled for fixed-view single-tree navigation.
     # if strafe != current[3] - current[1]:
     #     instructions.append("strafe {0}".format(strafe))
 
@@ -54,10 +53,8 @@ def process_LeWM_action(action_list, current):
     # if current[5] != action_list[5]:
     #     instructions.append("crouch {0}".format(action_list[5]))
 
-    # Re-issue attack every tick while attacking so Malmo sustains continuous mining;
-    # sending it once did not accumulate block-break progress. Release (0) only on change.
-    if action_list[7] != 0 or current[7] != action_list[7]:
-        instructions.append("attack {0}".format(int(action_list[7])))
+    if current[7] != action_list[7]:
+        instructions.append("attack {0}".format(action_list[7]))
 
     # Camera disabled for fixed-view navigation ablation.
     # if action_list[8] != current[8]:
